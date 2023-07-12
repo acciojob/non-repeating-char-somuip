d3ebd5ca126a811d4d525a1eae62416001889775
somuip <117620797+somuip@users.noreply.github.com>
2023-07-12 11:00:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function firstNonRepeatingChar(string){
+	let objFrequency = {};
+	for(let i=0;i<string.length;i++){
+		let char = string[i];
+		objFrequency[char] = (objFrequency[char] || 0) + 1;
+	}
+
+	for(let i=0;i<string.length;i++){
+		if(objFrequency[string[i]] === 1){
+			return string[i];
+		}
+	}
+	return null;
+}
+
+let string = prompt("Enter a string");
+let ans = firstNonRepeatingChar(string);
+
+if(ans !== null){
+	alert(ans);
+}
+else{
+	alert("no non repeating character in a string");
+}
+
